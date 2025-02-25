8f9fda87a6c3fcfd80298d3e8fbfe5a310bf23ce
Akshay Sihag <akshaysihag@outlook.com>
2025-02-25 20:05:44 +0530

    Fixed build errors

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  output: 'standalone',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    optimizeFonts: true,
+    optimizeImages: true,
+  },
+  images: {
+    domains: ['localhost'],
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
